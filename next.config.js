e739a26ede940107f477f0e3c2bb9f2614ee8138
Manuel Dugué <manuel.dugue@comvel.de>
2020-08-05 09:54:35 +0200

    added next-pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
-module.exports = {
-  poweredByHeader: false
-};
+const withPWA = require("next-pwa");
+
+// change start-url cache strategy, so that we can prompt user to reload when
+// new version available, instead of showing new version directly
+const runtimeCaching = require("next-pwa/cache");
+runtimeCaching[0].handler = "StaleWhileRevalidate";
+
+module.exports = withPWA({
+  poweredByHeader: false,
+  pwa: {
+    //disable: process.env.NODE_ENV === "development",
+    dest: "public",
+    register: false,
+    skipWaiting: false,
+    runtimeCaching
+  }
+});
