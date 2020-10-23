e11186e3524a18eda33baa210d864dee4120b61a
Manuel Dugué <manuel.dugue@comvel.de>
2020-10-23 11:53:24 +0200

    fixing print issue

--- pages/[[...index]].tsx
+++ pages/[[...index]].tsx
@@ -60,243 +60,245 @@ export default function Home(props: { document?: StructuredSheetProps }) {
   const isContainerHiddenInPrint = document != null;
   useEvaluateServiceWorker();
   return (
-    <div className="container">
-      <Head>
-        <title>Manuel Dugué</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.webmanifest"></link>
-      </Head>
-      <ClaimCard />
-      <ContactAside />
-      <LegalSection />
-      <ContactFooter />
+    <>
+      <div className="container">
+        <Head>
+          <title>Manuel Dugué</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="manifest" href="/manifest.webmanifest"></link>
+        </Head>
+        <ClaimCard />
+        <ContactAside />
+        <LegalSection />
+        <ContactFooter />
 
-      <style jsx global>{`
-        :root {
-          color-scheme: light dark;
-          --colorBodyBackground: #f7f7f7;
-          --colorBodyText: #fff;
-          --secondaryLink: #206c5f;
-          --legalLink: #bdbdbd;
-        }
-
-        @media (prefers-color-scheme: dark) {
+        <style jsx global>{`
           :root {
-            --colorBodyBackground: #080808;
-            --colorBodyText: #000;
-            --secondaryLink: #fff;
-            --legalLink: #545454;
+            color-scheme: light dark;
+            --colorBodyBackground: #f7f7f7;
+            --colorBodyText: #fff;
+            --secondaryLink: #206c5f;
+            --legalLink: #bdbdbd;
           }
-        }
 
-        .container {
-          display: flex;
-          flex-direction: column;
-          min-height: 100vh;
-        }
+          @media (prefers-color-scheme: dark) {
+            :root {
+              --colorBodyBackground: #080808;
+              --colorBodyText: #000;
+              --secondaryLink: #fff;
+              --legalLink: #545454;
+            }
+          }
 
-        @media print {
           .container {
-            display: ${isContainerHiddenInPrint ? "none" : "flex"};
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
           }
-        }
 
-        @media screen and (min-width: ${largeBreakpoint}) {
-          .container {
-            justify-content: space-between;
+          @media print {
+            .container {
+              display: ${isContainerHiddenInPrint ? "none" : "flex"};
+            }
           }
-        }
 
-        hgroup {
-          background: hsl(0 55% 62% / 1);
-          color: var(--colorBodyText);
-          padding: 10vmax 2vmax;
-          min-height: 55vh;
-          will-change: transform;
-        }
+          @media screen and (min-width: ${largeBreakpoint}) {
+            .container {
+              justify-content: space-between;
+            }
+          }
 
-        @media screen and (min-width: ${largeBreakpoint}) {
           hgroup {
-            margin: 10vh 0 0 10vw;
-            min-height: initial;
-            padding: 9vh 10vw 10vh;
-            width: 66vw;
+            background: hsl(0 55% 62% / 1);
+            color: var(--colorBodyText);
+            padding: 10vmax 2vmax;
+            min-height: 55vh;
+            will-change: transform;
           }
-        }
 
-        small {
-          font-size: max(1.5vmin, 1rem);
-          font-family: "Bungee Hairline", "SF Mono", "Ubuntu Mono", Consolas,
-            Menlo, monospace, cursive;
-          font-weight: 700;
-        }
+          @media screen and (min-width: ${largeBreakpoint}) {
+            hgroup {
+              margin: 10vh 0 0 10vw;
+              min-height: initial;
+              padding: 9vh 10vw 10vh;
+              width: 66vw;
+            }
+          }
 
-        .tagline {
-          font-family: "Bungee Inline", "SF Mono", "Ubuntu Mono", Consolas,
-            Menlo, monospace, cursive;
-          font-size: max(1.5vw, 1.25rem);
-          font-weight: normal;
-          margin-right: 2vw;
-          margin-top: 2vh;
-          will-change: transform;
-        }
+          small {
+            font-size: max(1.5vmin, 1rem);
+            font-family: "Bungee Hairline", "SF Mono", "Ubuntu Mono", Consolas,
+              Menlo, monospace, cursive;
+            font-weight: 700;
+          }
 
-        @media screen and (min-width: ${largeBreakpoint}) {
           .tagline {
-            color: hsl(47 80% 75% / 1);
-            font-size: 1.5vw;
+            font-family: "Bungee Inline", "SF Mono", "Ubuntu Mono", Consolas,
+              Menlo, monospace, cursive;
+            font-size: max(1.5vw, 1.25rem);
+            font-weight: normal;
+            margin-right: 2vw;
+            margin-top: 2vh;
+            will-change: transform;
           }
-        }
 
-        .dot {
-          background: var(--colorBodyBackground);
-          border-radius: 100%;
-          height: 4vmax;
-          position: absolute;
-          width: 4vmax;
-        }
-        .dotBR {
-          bottom: -2vmax;
-          right: -2vmax;
-        }
-        .dotBL {
-          bottom: -2vmax;
-          left: -2vmax;
-        }
-        .dotTR,
-        .dotTL {
-          visibility: hidden;
-        }
-        .dotTR {
-          right: -2vmax;
-          top: -2vmax;
-        }
-        .dotTL {
-          left: -2vmax;
-          top: -2vmax;
-        }
+          @media screen and (min-width: ${largeBreakpoint}) {
+            .tagline {
+              color: hsl(47 80% 75% / 1);
+              font-size: 1.5vw;
+            }
+          }
 
-        @media screen and (min-width: ${largeBreakpoint}) {
+          .dot {
+            background: var(--colorBodyBackground);
+            border-radius: 100%;
+            height: 4vmax;
+            position: absolute;
+            width: 4vmax;
+          }
+          .dotBR {
+            bottom: -2vmax;
+            right: -2vmax;
+          }
+          .dotBL {
+            bottom: -2vmax;
+            left: -2vmax;
+          }
           .dotTR,
           .dotTL {
-            visibility: initial;
+            visibility: hidden;
           }
-        }
+          .dotTR {
+            right: -2vmax;
+            top: -2vmax;
+          }
+          .dotTL {
+            left: -2vmax;
+            top: -2vmax;
+          }
 
-        aside {
-          color: hsl(47 80% 57% / 1);
-          font-family: "Bungee Inline", "SF Mono", "Ubuntu Mono", Consolas,
-            Menlo, monospace, cursive;
-          font-size: 1.125rem;
-          line-height: 1;
-          padding: 6vh 2vh 2vh;
-          text-align: center;
-        }
+          @media screen and (min-width: ${largeBreakpoint}) {
+            .dotTR,
+            .dotTL {
+              visibility: initial;
+            }
+          }
 
-        aside a {
-          color: var(--secondaryLink);
-          display: inline-block;
-          font-family: "Bungee Hairline", "SF Mono", "Ubuntu Mono", Consolas,
-            Menlo, monospace, cursive;
-          font-weight: 700;
-          padding: 1ch;
-        }
-        aside a:hover {
-          font-family: "Bungee Inline", "SF Mono", "Ubuntu Mono", Consolas,
-            Menlo, monospace, cursive;
-          font-weight: normal;
-        }
+          aside {
+            color: hsl(47 80% 57% / 1);
+            font-family: "Bungee Inline", "SF Mono", "Ubuntu Mono", Consolas,
+              Menlo, monospace, cursive;
+            font-size: 1.125rem;
+            line-height: 1;
+            padding: 6vh 2vh 2vh;
+            text-align: center;
+          }
 
-        .legal {
-          font-family: "Bungee Hairline", "SF Mono", "Ubuntu Mono", Consolas,
-            Menlo, monospace, cursive;
-          position: absolute;
-          right: 1rem;
-          display: flex;
-          align-items: flex-end;
-          justify-content: initial;
-        }
+          aside a {
+            color: var(--secondaryLink);
+            display: inline-block;
+            font-family: "Bungee Hairline", "SF Mono", "Ubuntu Mono", Consolas,
+              Menlo, monospace, cursive;
+            font-weight: 700;
+            padding: 1ch;
+          }
+          aside a:hover {
+            font-family: "Bungee Inline", "SF Mono", "Ubuntu Mono", Consolas,
+              Menlo, monospace, cursive;
+            font-weight: normal;
+          }
 
-        @media screen and (min-width: ${largeBreakpoint}) {
           .legal {
-            flex-direction: column;
+            font-family: "Bungee Hairline", "SF Mono", "Ubuntu Mono", Consolas,
+              Menlo, monospace, cursive;
+            position: absolute;
+            right: 1rem;
+            display: flex;
+            align-items: flex-end;
+            justify-content: initial;
           }
-        }
 
-        .legal a {
-          padding: 1ch 1ch 0;
-          font-weight: bold;
-          color: var(--legalLink);
-        }
+          @media screen and (min-width: ${largeBreakpoint}) {
+            .legal {
+              flex-direction: column;
+            }
+          }
 
-        .highlight-container {
-          bottom: 0;
-          left: 0;
-          overflow: hidden;
-          position: absolute;
-          right: 0;
-          top: 0;
-          zindex: -1;
-        }
+          .legal a {
+            padding: 1ch 1ch 0;
+            font-weight: bold;
+            color: var(--legalLink);
+          }
 
-        .highlight {
-          width: 30vmin;
-          height: 30vmin;
-          background: white;
-          position: absolute;
-          margin: -15vmin;
-          filter: blur(40vmin);
-          willchange: left, top;
-          transform: translate3d(0, 0, 0);
-        }
+          .highlight-container {
+            bottom: 0;
+            left: 0;
+            overflow: hidden;
+            position: absolute;
+            right: 0;
+            top: 0;
+            zindex: -1;
+          }
 
-        html,
-        body {
-          background: var(--colorBodyBackground);
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-          -webkit-font-smoothing: antialiased;
-          -moz-osx-font-smoothing: grayscale;
-          line-height: 1.25;
-          overflow: hidden;
-        }
+          .highlight {
+            width: 30vmin;
+            height: 30vmin;
+            background: white;
+            position: absolute;
+            margin: -15vmin;
+            filter: blur(40vmin);
+            willchange: left, top;
+            transform: translate3d(0, 0, 0);
+          }
 
-        svg {
-          stroke-width: 1.5px;
-        }
+          html,
+          body {
+            background: var(--colorBodyBackground);
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            line-height: 1.25;
+            overflow: hidden;
+          }
 
-        a {
-          text-decoration: none;
-          color: inherit;
-        }
+          svg {
+            stroke-width: 1.5px;
+          }
 
-        * {
-          box-sizing: border-box;
-        }
+          a {
+            text-decoration: none;
+            color: inherit;
+          }
 
-        ::-webkit-scrollbar {
-          width: 0.75rem;
-        }
+          * {
+            box-sizing: border-box;
+          }
 
-        ::-webkit-scrollbar-track {
-          background: #ffffff00;
-        }
-        ::-webkit-scrollbar-track:hover {
-          background: var(--colorBodyBackground);
-        }
+          ::-webkit-scrollbar {
+            width: 0.75rem;
+          }
 
-        ::-webkit-scrollbar-thumb {
-          border-radius: 2px;
-        }
-      `}</style>
+          ::-webkit-scrollbar-track {
+            background: #ffffff00;
+          }
+          ::-webkit-scrollbar-track:hover {
+            background: var(--colorBodyBackground);
+          }
+
+          ::-webkit-scrollbar-thumb {
+            border-radius: 2px;
+          }
+        `}</style>
+      </div>
       {document != null && (
         /* TODO: Lazy load */
         <StructuredSheet title={document.title} document={document.document} />
       )}
-    </div>
+    </>
   );
 }
 
