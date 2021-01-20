929dd36ee7a8609a2a84108cd7483d0ec8808ae5
Manuel Dugué <manuel.dugue@comvel.de>
2021-01-20 12:00:25 +0100

    bypassing cache for favicons valid starturl

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,22 +21,27 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href="/apple-touch-icon.png?v=wAM6anG0px"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href="/favicon-32x32.png?v=wAM6anG0px"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="/favicon-16x16.png?v=wAM6anG0px"
           />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="manifest" href="/site.webmanifest?v=wAM6anG0px" />
+          <link
+            rel="mask-icon"
+            href="/safari-pinned-tab.svg?v=wAM6anG0px"
+            color="#5bbad5"
+          />
+          <link rel="shortcut icon" href="/favicon.ico?v=wAM6anG0px" />
           <meta name="msapplication-TileColor" content="#6b9aa2" />
           <meta name="theme-color" content="#6b9aa2" />
           <link
