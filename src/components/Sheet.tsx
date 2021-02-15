5f4fdec578d71a7239ef8e5ccc516ec19f5e1517
Manuel Dugué <manuel.dugue@comvel.de>
2021-02-15 13:39:28 +0100

    font-display: swap -> optional

--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -93,7 +93,7 @@ export default function Sheet(props: SheetProps) {
           font-family: "Montserrat";
           font-style: normal;
           font-weight: 400;
-          font-display: swap;
+          font-display: optional;
           src: url(/fonts/montserrat-400.woff2) format("woff2");
           unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
             U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
@@ -103,7 +103,7 @@ export default function Sheet(props: SheetProps) {
           font-family: "Montserrat";
           font-style: normal;
           font-weight: 600;
-          font-display: swap;
+          font-display: optional;
           src: url(/fonts/montserrat-600.woff2) format("woff2");
           unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
             U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
