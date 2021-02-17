63e6c005e0a31a795fd9c76446e1b305fa7a140c
Manuel Dugué <manuel.dugue@comvel.de>
2021-02-17 09:41:23 +0100

    disabled next-pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const withPWA = require("next-pwa");
+/* TODO const withPWA = require("next-pwa"); */
 
 // change start-url cache strategy, so that we can prompt user to reload when
 // new version available, instead of showing new version directly
 const runtimeCaching = require("next-pwa/cache");
 runtimeCaching[0].handler = "StaleWhileRevalidate";
 
-module.exports = withPWA({
+module.exports = /* withPWA( */ {
   poweredByHeader: false,
   experimental: {
     optimizeFonts: true,
@@ -18,4 +18,4 @@ module.exports = withPWA({
     skipWaiting: false,
     runtimeCaching,
   },
-});
+} /* ) */;
