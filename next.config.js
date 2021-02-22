977e27612febac6c8344a3f3642b21873991fa6e
Manuel Dugué <manuel.dugue@comvel.de>
2021-02-22 09:18:43 +0100

    removed puppeteer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+withBundleAnalyzer({});
+
+module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   experimental: {
     optimizeFonts: true,
     reactMode: "concurrent",
   },
-};
+});
