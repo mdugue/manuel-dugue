b44a9b4f3fd7331ceb5fe42c20206590175f5567
Manuel Dugué <mdugue@gmail.com>
2021-02-23 12:28:22 +0100

    using preact (#22)
    Co-authored-by: Manuel Dugué <manuel.dugue@comvel.de>

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
+const withPreact = require("next-plugin-preact");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 withBundleAnalyzer({});
 
-module.exports = withBundleAnalyzer({
-  poweredByHeader: false,
-  experimental: {
-    optimizeFonts: true,
-    reactMode: "concurrent",
-  },
-});
+module.exports = withPreact(
+  withBundleAnalyzer({
+    poweredByHeader: false,
+    experimental: {
+      optimizeFonts: true,
+    },
+  })
+);
