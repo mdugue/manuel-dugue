8f823cdb3ff33be2a4c70b8410363b479b174952
Manuel Dugué <mail@manuel.fyi>
2022-12-28 08:49:56 +0100

    remove preact updated react & co

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
-const withPreact = require("next-plugin-preact");
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-withBundleAnalyzer({});
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+withBundleAnalyzer({})
 
-module.exports = withPreact(
-  withBundleAnalyzer({
-    webpack5: false,
-    poweredByHeader: false,
-    experimental: {
-      optimizeFonts: true,
-    },
-  })
-);
+withBundleAnalyzer({
+  poweredByHeader: false,
+})
