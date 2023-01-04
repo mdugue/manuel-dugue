79f5acae957af20679adeaad16ea9e9fdcad67fe
Manuel Dugué <mail@manuel.fyi>
2023-01-04 12:07:50 +0100

    added loading pages

---
+++ app/[sheet]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+	return (
+		<div className="py-4 text-lg font-inline text-gradient bg-gradient-to-tr from-yellow-500 to-yellow-300 mb-2 self-start">
+			loading…
+		</div>
+	)
+}
