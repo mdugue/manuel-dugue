7651c76878806bc4bdcc3f09a9554e1781d32493
Manuel Dugué <mail@manuel.fyi>
2023-05-10 14:48:28 +0200

    Dependency update (#196)
    * updated all dependencies
    
    * code format

--- util/notEmpty.ts
+++ util/notEmpty.ts
@@ -1,6 +1,6 @@
 /** removes empty elements from an array, so that TypeScript knows */
 export default function notEmpty<TValue>(
-  value: TValue | null | undefined
+	value: TValue | null | undefined,
 ): value is TValue {
-  return value != null
+	return value != null
 }
