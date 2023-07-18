f07f00ca79229c418942ad95bb330cd154d8dba8
Manuel Dugué <mail@manuel.fyi>
2023-07-18 15:06:52 +0200

    revalidate statically analyzable

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { i18n } from 'i18n-config'
 import { MetadataRoute } from 'next'
 
 export const runtime = 'edge'
-export const revalidate = 60 * 60 * 24 // 1 day
+export const revalidate = 86.4 // 60 * 60 * 24 1 day
 
 export default function sitemap(): MetadataRoute.Sitemap {
 	return i18n.locales.flatMap((locale) => [
