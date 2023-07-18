64a0c2c81aeafd3a7cc8ddd36e10318e080a1ff6
Manuel Dugué <mail@manuel.fyi>
2023-07-18 16:35:26 +0200

    added rootlayout

---
+++ app/layout.tsx
@@ -0,0 +1,7 @@
+/**
+ * Added Layout component to wrap all pages. This seems to avoid re-rendering the page when
+ * navigating between sub-pages, such as opening sheets
+ */
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return <>{children}</>
+}
