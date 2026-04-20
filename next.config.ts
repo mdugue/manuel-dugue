23f42602d74929a82716ce37a46a720d19db741e
Manuel Dugué <mail@manuel.fyi>
2026-04-20 19:52:38 +0200

    fixed broken pdf generation

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   cacheComponents: true,
   typedRoutes: true,
-  transpilePackages: ['@react-pdf/renderer'],
+  serverExternalPackages: ['@react-pdf/renderer'],
   experimental: {
     viewTransition: true,
   },
