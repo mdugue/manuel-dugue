a37e1ee58efa2198771eba2447cdb5b504ac7158
Manuel Dugué <mail@manuel.fyi>
2026-04-20 21:03:38 +0200

    added react compiler

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
   cacheComponents: true,
   typedRoutes: true,
   serverExternalPackages: ['@react-pdf/renderer'],
